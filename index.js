6e050503a635032389fbd016b142130a5f7fe163
viktorignatov <viktorignatov77@gmail.com>
2024-05-21 17:41:48 +0300

    Added main method and some TODO comments

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const fs = require('fs');
 const path = require('path');
 
 const configPath = path.join(__dirname, 'config.json');
-if (!fs.existsSync(configPath))
-    {
+if (!fs.existsSync(configPath)) {
         console.error('config.json file not found!');
         process.exit(1);
     }
 
 const {api_key} = require(configPath);
+
+async function main() {
+    //This is where the magic happens
+}
+
+main();
+
+//TODO: Caching, get exchange rate and saving the conversions to file
